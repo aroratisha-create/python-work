98d75ff83ad99e48061e0773fae98cf0fcc6b81d
aroratisha-create <arora.tisha@gmail.com>
2026-01-19 17:27:53 +0100

    Add files via upload

---
+++ and_or_not.py
@@ -0,0 +1,9 @@
+amaira=10
+pankaj=-10
+strawberry=-1
+if amaira<0 or strawberry<0:
+    print ("FEL SVAR!")
+elif pankaj<0 and strawberry<0:
+    print("RÄTT BRA JOBBAT, DU BLEV  !")
+else:
+    print("DU BLEV LURAD HAHAHAHAHHA")
