9e1c817661977ef290d06e4a237b77afb9c59089
aroratisha-create <arora.tisha@gmail.com>
2026-01-23 18:13:10 +0100

    Add files via upload

---
+++ 23_1_operators.py
@@ -0,0 +1,40 @@
+#maths=[4,5,6,7,8]
+#science=[4,5,6,7,8]
+
+#english=maths
+
+#print(maths is science) #False
+#print(maths is english) #True
+#print(maths is not science) #True
+#print(maths is not english) #False
+
+
+#a=3
+#b=4
+
+#if a is b:
+#    print("Incorrect!")
+#else:
+#    print("Correct")
+
+
+
+#a=[4,5,6,7,8]
+#b=[4,5,6,7,8]
+
+#c=a
+
+#print(4 in a)
+#print(c in b)
+#print(c in a)
+#print(a is c)
+
+a=3
+b=8
+print(3 & 8) #and
+print(3|8) #or
+print(~3) #not
+print(3^8) #xor
+print(3<<8) #left shift
+print(3>>8) #right shift
+
