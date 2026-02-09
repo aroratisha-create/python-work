1e1836ea0b6acc8b97b7da959d5c63d622a7e550
aroratisha-create <arora.tisha@gmail.com>
2026-02-09 17:06:18 +0100

    Add files via upload

---
+++ 2_9_loops.py
@@ -0,0 +1,16 @@
+#for r in range (1,10,2):
+ #   print(r)
+
+#a="AmAiRa"
+#for i in a:
+#    print(i)
+
+
+#n=int(input("I shall be small"))
+#sixseven=0
+#for i in ran,ge(1,n+1):
+ #   sixseven=sixseven+i
+ #   print(sixseven)
+
+for i in range(10,0,-1):
+    print(i)
