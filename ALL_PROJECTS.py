04a1a8987d7beb7cd30039cdce6a531fc7952248
aroratisha-create <arora.tisha@gmail.com>
2026-03-09 17:11:18 +0100

    Add files via upload

--- ALL_PROJECTS.py
+++ ALL_PROJECTS.py
@@ -1,54 +1,63 @@
-#n=int(input("CHOOSE A NUMBER??!"))
-#if n//2==0:
-#    print("Your number is even!")
-#else:
-#    print("Your number is odd!")
+# #n=int(input("CHOOSE A NUMBER??!"))
+# #if n//2==0:
+# #    print("Your number is even!")
+# #else:
+# #    print("Your number is odd!")
 
 
-#Raj1= 6
-#Raj2= 7
-#Raj3= 8
-#Raj4= 20
-#Raj5= 28
+# #Raj1= 6
+# #Raj2= 7
+# #Raj3= 8
+# #Raj4= 20
+# #Raj5= 28
 
-#print (Raj1,Raj2,Raj3,Raj4,Raj5)
-
-
-#a="Congratalations!"
-#print(a.upper())
+# #print (Raj1,Raj2,Raj3,Raj4,Raj5)
 
 
-#n=int(input("Choose a number??"))
-#print(n**0.5)
-
-#import turtle as t
-#t.forward(90)
-#t.right(90)
-#t.forward(90)
-#t.right(90)
-#t.forward(90)
-#t.right(90)
-#t.forward(90)
-#t.right(90)
-#t.done()
+# #a="Congratalations!"
+# #print(a.upper())
 
 
-#age=int(input("How old are you??"))
-#
-#if age<=20:
-#    if age>=10:
-#        print("You are accepted in ages between 10 and 20!")
+# #n=int(input("Choose a number??"))
+# #print(n**0.5)
+
+# #import turtle as t
+# #t.forward(90)
+# #t.right(90)
+# #t.forward(90)
+# #t.right(90)
+# #t.forward(90)
+# #t.right(90)
+# #t.forward(90)
+# #t.right(90)
+# #t.done()
 
 
-#w=int (input("How many degrees celcius is the weather?"))
-#
-#if w>=17:
-#    print ("Wear light and soft clothes!")
-#else:
-#    print ("Wear jackets and pullovers!")
+# #age=int(input("How old are you??"))
+# #
+# #if age<=20:
+# #    if age>=10:
+# #        print("You are accepted in ages between 10 and 20!")
 
-c=str(input("Choose a character,numbers or letters?"))
-if c.isalpha():
-    print("You have chosen a letter")
-else:
-    print("You have not chosen a letter")
+
+# #w=int (input("How many degrees celcius is the weather?"))
+# #
+# #if w>=17:
+# #    print ("Wear light and soft clothes!")
+# #else:
+# #    print ("Wear jackets and pullovers!")
+
+# c=str(input("Choose a character,numbers or letters?"))
+# if c.isalpha():
+#     print("You have chosen a letter")
+# else:
+#     print("You have not chosen a letter")
+
+
+row=20
+for py in range (1,row+1):
+    for j in range (row-py):
+        print(" ",end="")
+    for k in range (py):
+        print("*",end="")
+    print()
