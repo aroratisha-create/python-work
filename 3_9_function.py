b795a2f957c627a96ced798814385ba229690673
aroratisha-create <arora.tisha@gmail.com>
2026-03-09 17:03:15 +0100

    Add files via upload

---
+++ 3_9_function.py
@@ -0,0 +1,52 @@
+#def a ():
+#    print ("HELLOOOOOO")
+#    print("HRUUUU")
+#    print("WSPPPP")
+          
+#a()
+#a()
+
+# def add(p,q):
+#     return p+q
+
+# def subtract(p,q):
+#     return p-q
+
+# def multiply(p,q):
+#     return p*q
+
+# def division(p,q):
+#     return p/q
+
+# print ("Please select the operation.")
+
+# print ("a. Add")
+
+# print ("b. Subtract")
+
+# print ("c. Multiply")
+
+# print ("d. Divide")
+
+
+# choice = input("Please enter choice (a/ b/ c/ d): ")
+
+
+# num_1 = int (input ("Please enter the first number: "))
+
+# num_2 = int (input ("Please enter the second number: "))
+
+# if choice =="a":
+#     print(add(num_1,num_2))
+
+# elif choice=="b":
+#     print(subtract(num_1,num_2))
+
+# elif choice=="c":
+#     print(multiply(num_1,num_2))
+
+# elif choice=="d":
+#     print(division(num_1,num_2))
+
+# else:
+#     print("Invalid")
