b139a6dcfd1233d933cfaf8d68e2527d25ddcb44
aroratisha-create <arora.tisha@gmail.com>
2026-03-20 17:18:30 +0100

    Add files via upload

---
+++ 3_20_keywords.py
@@ -0,0 +1,34 @@
+# a=input("Enter a word?")
+# for i in a:
+#     if(i == "A"):
+#         print ("A is found")
+#         break
+#     else:
+#         print ("A is not found")
+
+
+# for x in range(21):
+#     if x%20==0:
+#         print("Rubiks cube")
+
+#     elif x % 15==0:
+#         pass
+
+#     elif x % 5 == 0:
+#         print("Drink")
+
+#     elif x % 3 == 0:
+#         print ("Bubbles")
+#     else:
+#         print(x)
+
+
+
+# var=10
+# while var>0:
+#     var=var - 1
+#     if var==5:
+#         continue 
+#     print('\nCurrent variable value:',var)
+# print("\nGood bye!")
+
