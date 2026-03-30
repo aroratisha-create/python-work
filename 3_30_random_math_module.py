1eb6c41c67380b29c423a96b66104c984f06da87
aroratisha-create <arora.tisha@gmail.com>
2026-03-30 18:05:20 +0200

    Add files via upload

---
+++ 3_30_random_math_module.py
@@ -0,0 +1,33 @@
+# import random 
+# playing = True
+# number = str(random.randint(0,9)) 
+
+# print("I will generate a number from 0 to 9, and you have to guess the number one digit at a time.")
+# print("The game ends when you get 1 hero!")
+
+# while playing:
+#     guess = input("Give me your best guess! \n")
+#     if number == guess:
+#         print("You win the game")
+#         print("The number was",number)
+#         break
+
+# else:
+#     print("Your guess isn't quite right, try again. \n")
+
+
+# rock paper scissors aso works here in this topic,thogh we have done it before!
+
+
+import math 
+print('The Floor and Ceiling value of 23.56 are: ' + str(math.ceil(23.56)) + ', ' + 
+str(math.floor(23.56)))
+
+x = 10
+y = -15
+print('The value of x after copying the sign from y is: ' + str(math.copysign(x, y)))
+
+print('Absolute value of -96 and 56 are: ' + str(math.fabs(-96)) + ', ' +
+ str(math.fabs(56)))
+
+print('The GCD of 24 and 56 : ' + str(math.gcd(24, 56)))
